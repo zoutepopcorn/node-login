b4795dd765ea41a88d4efc55dd75c2730ccd759b
zoutepopcorn <zoutepopcorn@hotmail.com>
2017-07-18 15:51:48 +0200

    Update emailValidator.js

--- app/public/js/form-validators/emailValidator.js
+++ app/public/js/form-validators/emailValidator.js
@@ -12,8 +12,8 @@ function EmailValidator()
 
 EmailValidator.prototype.validateEmail = function(e)
 {
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(e);
+	//var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return true;
 }
 
 EmailValidator.prototype.showEmailAlert = function(m)
